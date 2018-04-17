85a221b7476bdd6c514c0beb30d95e5aab42a7a4
DESKTOP-61GEORI\Breanne <simo0133quinlive.com>
2018-04-17 11:47:13 -0400

    my code

---
+++ src/MyProGisBLL/IMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGisBLL
+{
+    public interface IMap
+    {
+        string Name { get; set; }
+        ILayer[] Layers { get; }
+        int LayerCount { get; }
+        void AddLayer(ILayer layer);
+        void RemoveLayer(int index);
+    }
+}
